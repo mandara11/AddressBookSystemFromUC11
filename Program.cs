24a4c701e86705ab3bd4e1014c385c7590c4dfd7
mandara11 <mandaram2345@gmail.com>
2022-08-26 04:50:35 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookSystemFromUC11
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System Problem");
+        }
+    }
+}
